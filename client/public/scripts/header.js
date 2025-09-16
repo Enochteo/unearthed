b7401c01f799a62458edbb668c354ea85b17f7de
Enochteo <enochowoade@gmail.com>
2025-09-15 23:05:12 -0500

    create client header

--- client/public/scripts/header.js
+++ client/public/scripts/header.js
@@ -5,3 +5,29 @@ headerContainer.className = 'header-container'
 
 const headerLeft = document.createElement('div')
 headerLeft.className = 'header-left'
+
+const headerLogo = document.createElement('img')
+headerLogo.src = '/logo.png'
+
+const headerTitle = document.createElement('h1')
+headerTitle.textContent = 'UnEarthed'
+
+headerLeft.appendChild(headerLogo)
+headerLeft.appendChild(headerTitle)
+
+const headerRight = document.createElement('div')
+headerRight.className = 'header-right'
+
+const headerButton = document.createElement('div')
+headerButton.textContent = 'Home'
+
+headerButton.addEventListener('click', function hadleClick (event) {
+    window.location = '/'
+})
+
+headerRight.appendChild(headerButton)
+
+headerContainer.appendChild(headerLeft)
+headerContainer.appendChild(headerRight)
+
+header.appendChild(headerContainer)
