8ce98675dd79ba3b034df218c463d3399434c670
Enochteo <enochowoade@gmail.com>
2025-09-24 20:28:51 -0500

    add pg setup

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-import express from 'express'
-import giftsRouter from './routes/gifts.js'
+import express from "express";
+import giftsRouter from "./routes/gifts.js";
+import "./config/dotenv.js";
+import "./config/dotenv.js";
 
+const app = express();
+app.use("/public", express.static("./public"));
+app.use("/scripts", express.static("./public/scripts"));
+app.use("/gifts", giftsRouter);
 
-const app = express()
-app.use('/public', express.static('./public'))
-app.use('/scripts', express.static('./public/scripts'))
-app.use('/gifts', giftsRouter)
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      '<h1 style="text-align: center; margin-top: 50px;"> UnEarthed API</h1>'
+    );
+});
 
-
-app.get('/', (req, res) => {
-    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;"> UnEarthed API</h1>')
-})
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`)
-})
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
