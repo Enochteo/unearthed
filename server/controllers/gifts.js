f7f30384614b04638e9c6ad087d06eb5216df47b
Enochteo <enochowoade@gmail.com>
2025-10-01 20:36:03 -0500

    add gift controller and router

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -9,6 +9,17 @@ const getGifts = async (req, res) => {
   }
 };
 
+const getGiftsbyId = async (req, res) => {
+  try {
+    const selectQuery = `SELECT id, name, pricePoint, audience, image, description FROM gifts WHERE id = $1`;
+    const giftId = req.params.giftId;
+    const results = await pool.query(selectQuery, [giftId]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
 export default {
   getGifts,
+  getGiftsbyId,
 };
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -4,15 +4,12 @@ import { fileURLToPath } from "url";
 //import giftData from "../data/gifts.js";
 import GiftController from "../controllers/gifts.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
 router.get("/", GiftController.getGifts);
 
-router.get("/:giftId", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "../public/gift.html"));
-});
-
+router.get("/:giftId", GiftController.getGiftsbyId);
 export default router;
